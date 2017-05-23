8e830f5a2c31929307df497ce2bbe1e5acf2ec57
AVINASH <AVINASH@AVINASH-PC>
2017-05-23 18:07:20 +0530

    by second

---
+++ ruff/src/ruff/StringRuff.java
@@ -0,0 +1,17 @@
+package ruff;
+
+public class StringRuff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1="avi";
+System.out.println(s1);
+String s2=s1.concat("gopi");
+System.out.println(s2);
+System.out.println(s1);
+s1=s1.concat("jfgjf");
+System.out.println(s1);
+System.out.println("huuuuu");
+	}
+
+}
